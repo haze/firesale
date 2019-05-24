1503c1513855fb5d827f482ec3218749e5433ffe
Haze Booth <isnt@haze.cool>
2019-05-24 15:23:05 -0400

    can actually ready documents now

--- src/api.rs
+++ src/api.rs
@@ -7,10 +7,15 @@ use goauth::auth::JwtClaims;
 use goauth::scopes::Scope;
 use anymap::AnyMap;
 use std::collections::HashMap;
-use json::JsonValue;
+use json::{JsonValue, JsonError};
+use goauth::scopes::Scope::Firebase;
+use std::convert::TryFrom;
 
 const FIRESTORE_BASE_URL: &'static str = "https://firestore.googleapis.com/v1";
 const TOKEN_URL: &'static str = "";
+
+// the `fields` attribute for Firestore Documents
+pub type FirestoreFields = HashMap<String, FirestoreType>;
 
 // A Firestore document
 #[derive(Debug)]
@@ -18,18 +23,7 @@ pub struct Document {
     pub name: String,
     pub create_time: DateTime<Utc>,
     pub update_time: DateTime<Utc>,
-    pub data: AnyMap,
-}
-
-// A value used in Firestore responses
-// i.e
-// fields: {
-//   speed: {
-//     "integerValue": "100"
-//   }
-// }
-struct Value {
-
+    pub fields: FirestoreFields,
 }
 
 #[derive(Deserialize, Debug)]
@@ -39,6 +33,17 @@ struct PartialDocument {
     pub update_time: DateTime<Utc>,
 }
 
+impl PartialDocument {
+    fn assemble(&self, fields: FirestoreFields) -> Document {
+        let name = self.name.clone(); // FIXME(hazebooth): clones are bad :(
+        let create_time = self.create_time.clone();
+        let update_time = self.update_time.clone();
+        Document {
+            name, create_time, update_time, fields,
+        }
+    }
+}
+
 // Represents a contextualized instance for database handling
 #[derive(Debug)]
 pub struct DatabaseContext {
@@ -46,16 +51,159 @@ pub struct DatabaseContext {
     auth_token: goauth::auth::Token
 }
 
-fn map_firestore_fields_to_any_map(fields: &json::JsonValue)
--> AnyMap {
-    let any_map = AnyMap::new();
+// Firestore GeoPoint type
+#[derive(Debug, Clone, Copy)]
+struct GeoPoint {
+    latitude: i32,
+    longitude: i32,
+}
+
+// Represents a mapping between Firestore data types and Rust types
+#[derive(Debug)]
+enum FirestoreType {
+    Integer(i32),
+    Boolean(bool),
+    String(String),
+    GeoLocation(GeoPoint),
+    Array(Vec<FirestoreType>),
+    Map(FirestoreFields),
+    Timestamp(DateTime<Utc>),
+    Null
+}
+
+impl FirestoreType {
+    fn extract_integer(value: &JsonValue) -> Result<i32, String> {
+        let value_str = value["integerValue"].as_str()
+            .ok_or_else(|| String::from("No string value found"))?.to_string();
+        value_str.parse::<i32>()
+            .map_err(|_| String::from("Failed to parse string value to integer"))
+    }
+
+    fn extract_boolean(value: &JsonValue) -> Result<bool, String> {
+        value["booleanValue"].as_bool()
+            .ok_or_else(|| String::from("Found no boolean value"))
+    }
+
+    fn extract_geopoint(value: &JsonValue) -> Result<GeoPoint, String> {
+        let latitude = value["geoPointValue"]["latitude"].as_i32()
+            .ok_or_else(|| String::from("Failed to parse latitude"))?;
+        let longitude = value["geoPointValue"]["longitude"].as_i32()
+            .ok_or_else(|| String::from("Failed to parse longitude"))?;
+        Ok(GeoPoint{
+            latitude, longitude
+        })
+    }
+
+    fn extract_string(value: &JsonValue) -> Result<String, String> {
+        value["stringValue"].as_str()
+            .ok_or_else(|| String::from("Failed to get stringValue string"))
+            .map(String::from)
+    }
+
+    fn extract_timestamp(value: &JsonValue) -> Result<DateTime<Utc>, String> {
+        let timestamp_str = value["timestampValue"].as_str()
+            .ok_or_else(|| String::from("Failed to get timestamp string"))?;
+        timestamp_str.parse::<DateTime<Utc>>()
+            .map_err(|e| format!("Failed to parse timestamp: {}", e))
+    }
+
+    fn extract_array(value: &JsonValue) -> Result<Vec<FirestoreType>, String> {
+        let values = &value["arrayValue"]["values"];
+        let array_len = values.len();
+        let mut firestore_values = Vec::with_capacity(array_len);
+        for item in values.members() {
+            firestore_values.push(FirestoreType::try_from(item)?);
+        }
+        Ok(firestore_values)
+    }
+
+    fn extract_map(value: &JsonValue) -> Result<FirestoreFields, String> {
+        let mut fields = FirestoreFields::new();
+        let map_value = &value["mapValue"]["fields"];
+        for (key, entry ) in map_value.entries() {
+            let parsed_value = FirestoreType::try_from(entry)?;
+            fields.insert(key.to_string(), parsed_value);
+        }
+        Ok(fields)
+    }
+
+}
+
+impl TryFrom<&JsonValue> for FirestoreType {
+    type Error = String;
+
+    fn try_from(value: &JsonValue) -> Result<Self, Self::Error> {
+        if is_integer(value) {
+            let value = FirestoreType::extract_integer(value)?;
+            return Ok(FirestoreType::Integer(value))
+        } else if is_string(value) {
+            let value = FirestoreType::extract_string(value)?;
+            return Ok(FirestoreType::String(value))
+        } else if is_boolean(value) {
+            let value = FirestoreType::extract_boolean(value)?;
+            return Ok(FirestoreType::Boolean(value))
+        } else if is_null(value) {
+            return Ok(FirestoreType::Null)
+        } else if is_geopoint(value) {
+            let value = FirestoreType::extract_geopoint(value)?;
+            return Ok(FirestoreType::GeoLocation(value))
+        } else if is_timestamp(value) {
+            let value = FirestoreType::extract_timestamp(value)?;
+            return Ok(FirestoreType::Timestamp(value))
+        } else if is_array(value) {
+            let values = FirestoreType::extract_array(value)?;
+            return Ok(FirestoreType::Array(values))
+        } else if is_map(value) {
+            let values = FirestoreType::extract_map(value)?;
+            return Ok(FirestoreType::Map(values))
+        }
+        Err("No value found".to_string())
+    }
+}
+
+fn is_integer(value: &JsonValue) -> bool {
+    value.has_key("integerValue")
+}
+
+fn is_boolean(value: &JsonValue) -> bool {
+    value.has_key("booleanValue")
+}
+
+fn is_null(value: &JsonValue) -> bool {
+    value.has_key("nullValue")
+}
+
+fn is_geopoint(value: &JsonValue) -> bool {
+    value.has_key("geoPointValue")
+}
+
+fn is_timestamp(value: &JsonValue) -> bool {
+    value.has_key("timestampValue")
+}
+
+fn is_string(value: &JsonValue) -> bool {
+    value.has_key("stringValue")
+}
+
+fn is_map(value: &JsonValue) -> bool {
+    value.has_key("mapValue")
+}
+
+fn is_array(value: &JsonValue) -> bool {
+    value.has_key("arrayValue")
+}
+
+
+fn get_firestore_fields(fields: &json::JsonValue)
+-> Result<FirestoreFields, String> {
+    let mut field_map = FirestoreFields::new();
     if fields.is_object() {
         for (name, field) in fields.entries() {
-            // is integer?
-            println!("{} = {:#}", name, field);
+            let fsv = FirestoreType::try_from(field)?;
+            field_map.insert(name.to_string(), fsv);
         }
     }
-    any_map
+    Ok(field_map)
 }
 
 impl DatabaseContext {
@@ -106,17 +254,14 @@ impl DatabaseContext {
             .header(reqwest::header::AUTHORIZATION, &*self.get_authorization_key())
             .send().map_err(|_| "Failed to get response from Google")?;
         let response_text = response.text().map_err(|_| "Failed to get response text")?;
-//        let partial_doc: PartialDocument = response.json().map_err(|e| format!("Failed to convert response to json: {}", e))?;
-
+        println!("{}", response_text);
         let parsed_json = json::parse(&*response_text)
             .map_err(|_| "Failed to parse request json (text)")?;
-        dbg!(&parsed_json);
         let fields = &parsed_json["fields"];
-        dbg!(&fields);
-        map_firestore_fields_to_any_map(fields);
-        Ok(())
-
+        let fields = get_firestore_fields(fields)?;
 //        let assembled_doc = partial_doc.assemble(fields);
+//        Ok(assembled_doc)
+        Ok(())
     }
 
     // Used to give us the key for our Authorization Header
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,29 @@ use libfiresale::api::DatabaseContext;
 // basic 1.0 support
 // read document path
 
+const GOOGLE_APPLICATION_CREDENTIALS_KEY: &'static str = "GOOGLE_APPLICATION_CREDENTIALS";
+const PROJECT_ID_KEY: &'static str = "PROJECT_ID";
+
+#[derive(Debug, Clone)]
+struct Environment {
+    pub service_account_path: String,
+    pub project_id: String,
+}
+
+fn probe_env() -> Result<Environment, String> {
+    use std::env;
+    let service_account_path = env::var(GOOGLE_APPLICATION_CREDENTIALS_KEY)
+        .map_err(|_| format!("Could not find application credentials, is {} set?", GOOGLE_APPLICATION_CREDENTIALS_KEY))?;
+    let project_id = env::var(PROJECT_ID_KEY)
+        .map_err(|_| format!("Could not find project id, is {} set?", PROJECT_ID_KEY))?;
+    Ok(Environment{
+        service_account_path, project_id
+    })
+}
+
 fn main() -> Result<(), String> {
-    let service_account = "./service_account.json";
-    let project_id = "hazes-test-project";
-    let context = DatabaseContext::new(project_id, service_account)
+    let environment = probe_env()?;
+    let context = DatabaseContext::new(environment.project_id, environment.service_account_path)
         .map_err(|_| "Failed to acquire database context")?;
     dbg!(context.get_document("cars", "bmw"));
 //    dbg!(document);
